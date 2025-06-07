c283daf00f22f16bfb7394300c7c1660fdc46ab8
Cesar Castillo <42988618+sgnclexus@users.noreply.github.com>
2025-06-06 22:36:11 -0600

    Updated TasksPage.tsx

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -196,9 +196,9 @@ const TasksPage: React.FC = () => {
               </CardHeader>
               <CardContent className="p-3">
                 <Droppable droppableId="pending">
-                  {
-                    
-                    (provided, snapshot) => (                      
+                  {(provided, snapshot) => 
+                  console.log("✅ Rendering Droppable: pending");
+                  return (                      
                     <div
                       {...provided.droppableProps}
                       ref={provided.innerRef}
@@ -240,7 +240,8 @@ const TasksPage: React.FC = () => {
                       ) }
                       {provided.placeholder}
                     </div>
-                  )}
+                  );
+                  }}
                 </Droppable>
               </CardContent>
             </Card>
